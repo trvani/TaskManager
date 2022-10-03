2c907f4e737c65f500a92508d67d95e126635f5c
trvani <47416856+trvani@users.noreply.github.com>
2022-10-03 23:59:46 +0300

    Add project files.

---
+++ TaskManager/Models/Task.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models
+{
+    public class Task
+    {
+        [Key]
+        public int id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ TaskManager/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+        public string email { get; set; }
+    }
+}
