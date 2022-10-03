34a80901d6463398fee76f5cf41085138cca0e35
trvani <47416856+trvani@users.noreply.github.com>
2022-10-04 00:01:48 +0300

    CreateTask Model and user model

--- TaskManager/Models/Task.cs
+++ TaskManager/Models/Task.cs
@@ -6,10 +6,10 @@ namespace TaskManager.Models
     {
         [Key]
         public int id { get; set; }
-
         public string Title { get; set; }
         public string Description { get; set; }
         public string Type { get; set; }
-        public string Title { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime DueDate { get; set; }
     }
 }
