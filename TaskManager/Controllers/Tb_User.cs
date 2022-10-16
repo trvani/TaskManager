b156d0a2b7369128c736adf8064ea1d2f964d78d
trvani <47416856+trvani@users.noreply.github.com>
2022-10-16 23:44:12 +0300

    Firebase implementation to core app

---
+++ TaskManager/Controllers/Tb_User.cs
@@ -0,0 +1,16 @@
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManager.Controllers
+{
+    public class Tb_User : Controller
+    {
+        IFirebaseConfig Config = new FirebaseConfig();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
